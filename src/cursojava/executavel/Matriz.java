a38a8a9434b480b56ad1f216bbef06a07eaa4b4a
Lucas Mocroski <devmocroski@gmail.com>
2022-10-09 17:08:06 -0300

    "Criando uma Matriz multidimensional"

---
+++ src/cursojava/executavel/Matriz.java
@@ -0,0 +1,21 @@
+package cursojava.executavel;
+
+public class Matriz {
+    public static void main(String[] args) {
+
+        int notas[] [] = new int[1] [3]; /*matriz*/
+
+        notas[0] [0] = 80; /*na primeira linha e primeira coluna o valor é 80*/
+        notas[0] [1] = 90;
+        notas[0] [2] = 70;
+
+        notas[1] [0] = 40;
+        notas[1] [1] = 60;
+        notas[1] [2] = 30;
+
+        System.out.println(notas);
+
+
+
+    }
+}
