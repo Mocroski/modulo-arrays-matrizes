bb6d1cc74e83a2e3475482f484062dfa037fc32e
Lucas Mocroski <devmocroski@gmail.com>
2022-10-10 15:57:13 -0300

    "Processamento concorrente entre duas thread"

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -31,6 +31,34 @@ public class AulaThread {
         }.start();/*start liga a thread que ifca processando paralelamente*/
 
 
+                    /*DIVISAO DAS THREADS*/
+
+
+        /*thread processsando em paralelo*/
+        new Thread() {
+
+            @Override
+            public void run() {/*executa o qe queremos*/
+
+                /*codigo da rotina*/
+                /*codigo que executa em paralelo*/
+                for(int pos =0; pos < 10; pos ++) {
+
+                    /*quero executar essa rotina com tempo de para ou com tempo determinado*/
+                    System.out.println("executando alguma rotina 2");
+
+                    try {
+                        Thread.sleep(2000);/*coloca tempo na execução, precisa de uma exeption*/
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+                System.out.println("chegou ao fim do teste de thread");
+                /*fim do codigo em paralelo*/
+            }
+        }.start();/*start liga a thread que ifca processando paralelamente*/
+
 
         /*fluxo do sistema, cadastro de venda, emissao de nota fiscal etc*/
         JOptionPane.showMessageDialog(null,"sistema continua executando para o usuario");
