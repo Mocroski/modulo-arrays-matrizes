5a8f4a7c80b6d006abc1dc0f307b2afd4ac4ea6c
Lucas Mocroski <devmocroski@gmail.com>
2022-10-10 13:02:15 -0300

    "Criando a primeira Thread"

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -1,17 +1,39 @@
 package cursojava.thread;
 
+import javax.swing.*;
+
 public class AulaThread {
     public static void main(String[] args) throws  InterruptedException{
 
-        for(int pos =0; pos < 10; pos ++) {
+        /*thread processsando em paralelo*/
+        new Thread() {
 
-            /*quero executar essa rotina com tempo de para ou com tempo determinado*/
-            System.out.println("executando alguma rotina:");
+            @Override
+            public void run() {/*executa o qe queremos*/
 
-            Thread.sleep(1000);/*coloca tempo na execução, precisa de uma exeption*/
+                /*codigo da rotina*/
+                /*codigo que executa em paralelo*/
+                for(int pos =0; pos < 10; pos ++) {
 
-        }
-        System.out.println("chegou ao fim do teste de thread");
+                    /*quero executar essa rotina com tempo de para ou com tempo determinado*/
+                    System.out.println("executando alguma rotina:");
+
+                    try {
+                        Thread.sleep(1000);/*coloca tempo na execução, precisa de uma exeption*/
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+                System.out.println("chegou ao fim do teste de thread");
+                /*fim do codigo em paralelo*/
+            }
+        }.start();/*start liga a thread que ifca processando paralelamente*/
+
+
+
+        /*fluxo do sistema, cadastro de venda, emissao de nota fiscal etc*/
+        JOptionPane.showMessageDialog(null,"sistema continua executando para o usuario");
 
     }
 }
